21ee7f2b7ca43208e36cfba3908d1013160929cb
deraldo.palomino <deraldo.palomino@hotmail.com>
2022-11-10 17:24:43 -0300

    Atualização do codigo.

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-
-class funcionarios:
+class Funcionarios:
 
     def __int__(self):
+        self.lista = []
         self.id
         self.nome
-        self.departamento
+        self.setor
+        self.salario
+        self.lista.append(self.id, self.nome, self.setor, self.salario)
+
+    def criar_funcionario(self, nome, setor, salario):
+        try:
+            nome = str(nome)
+            setor = str(setor)
+            salario = float(salario)
+        except:
+            pass
+        else:
+            pass
 
 
+class Interface(Funcionarios):
+
+    def __init__(self):
+        self.control = ''
+
+    def loop(self):
+        self.home()
+        while True:
+            break
+
+    def home(self):
+        print()
 
 
 if __name__ == '__main__':
+    screen = Interface()
+    screen.loop()
